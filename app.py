02ce4704286a127c5cc0db592f4b1e4c9228e938
atvars <avelavs@gmail.com>
2021-01-31 14:27:01 +0000

    added register.html and made registration fully functional

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -22,6 +23,30 @@ mongo = PyMongo(app)
 def get_dogs():
     dogs = mongo.db.dogs.find()
     return render_template("dogs.html", dogs=dogs)
+
+
+# Registering new user
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        # check if username already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+        #if username is already taken
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+        #reistering using workzeug password hashing
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
+    return render_template("register.html")
 
 
 if __name__ == "__main__":
