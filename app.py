3f2022ec9693d8d75a96c9c8d0d343e79ecb4828
atvars <avelavs@gmail.com>
2021-01-31 18:10:32 +0000

    users can log in, log out, and see their profile

--- app.py
+++ app.py
@@ -32,11 +32,11 @@ def register():
         # check if username already exists in db
         existing_user = mongo.db.users.find_one(
             {"username": request.form.get("username").lower()})
-        #if username is already taken
+        # if username is already taken
         if existing_user:
             flash("Username already exists")
             return redirect(url_for("register"))
-        #reistering using workzeug password hashing
+        # reistering using workzeug password hashing
         register = {
             "username": request.form.get("username").lower(),
             "password": generate_password_hash(request.form.get("password"))
@@ -46,9 +46,11 @@ def register():
         # put the new user into 'session' cookie
         session["user"] = request.form.get("username").lower()
         flash("Registration Successful!")
+        return redirect(url_for("profile", username=session["user"]))
     return render_template("register.html")
 
-#Log In
+
+# Log In
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
@@ -62,6 +64,7 @@ def login():
                 existing_user["password"], request.form.get("password")):
                 session["user"] = request.form.get("username").lower()
                 flash("Welcome, {}".format(request.form.get("username")))
+                return redirect(url_for("profile", username=session["user"]))
             else:
                 # invalid password match
                 flash("Incorrect Username and/or Password")
@@ -73,6 +76,30 @@ def login():
             return redirect(url_for("login"))
 
     return render_template("login.html")
+
+
+# profiles page
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    # session username from db
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    # 
+    if session["user"]:
+        return render_template("profile.html", username=username)
+
+    return redirect(url_for("login"))
+
+
+# log out
+@app.route("/logout")
+def logout():
+    # remove user from session cookie
+    flash("You have been logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
+
+
 
 
 if __name__ == "__main__":
