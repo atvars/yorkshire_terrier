12c26cb66d4ac20e55f7438d202316ae063d6b17
atvars <avelavs@gmail.com>
2021-01-31 23:16:30 +0000

    set up add dog form and render all cities from mongoDB

--- app.py
+++ app.py
@@ -62,9 +62,11 @@ def login():
             # ensure hashed password matches user input
             if check_password_hash(
                 existing_user["password"], request.form.get("password")):
-                session["user"] = request.form.get("username").lower()
-                flash("Welcome, {}".format(request.form.get("username")))
-                return redirect(url_for("profile", username=session["user"]))
+                    session["user"] = request.form.get("username").lower()
+                    flash("Welcome, {}".format(
+                        request.form.get("username")))
+                    return redirect(url_for(
+                        "profile", username=session["user"]))
             else:
                 # invalid password match
                 flash("Incorrect Username and/or Password")
@@ -100,6 +102,10 @@ def logout():
     return redirect(url_for("login"))
 
 
+@app.route("/add_dog")
+def add_dog():
+    cities = mongo.db.cities.find().sort("cities_name", 1)
+    return render_template("add_dog.html", cities=cities)
 
 
 if __name__ == "__main__":
--- static/js/script.js
+++ static/js/script.js
@@ -2,3 +2,18 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
   });
+
+// for adding your dog 
+$(document).ready(function(){
+    $('.collapsible').collapsible();
+  });
+
+// to see if which dogs have found their match
+$(document).ready(function(){
+    $(".tooltipped").tooltip();
+  });
+
+// to initialize scripts
+$(document).ready(function(){
+    $('select').formSelect();
+  });
